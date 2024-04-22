468127d5e228fb6ebb3e4309e4ed29095867f7ac
Giuseppe Cattano <giuseppe.cattano@gmail.com>
2024-04-22 20:30:48 +0200

    completato esercizio variabili

---
+++ main.js
@@ -0,0 +1,14 @@
+let num1 = 34;
+let num2 = 76;
+console.log(`Il valore della variabile num1 e' ${num1}`);
+console.log(`Il valore della variabile num2 e' ${num2}`);
+let stringa = 'fallout';
+console.log(`Il valore della variabile stringa e' ${stringa}`);
+stringa = "world of warcraft";
+console.log(`Il valore della variabile stringa e' ${stringa}`);
+const PIGRECO = 3.14159265359;
+console.log(`il valore di PIGRECO e' ${PIGRECO}`);
+PIGRECO = 1;
+// Errore restituito dalla console:
+// Uncaught TypeError: Assignment to constant variable.
+//     at main.js:11:9
